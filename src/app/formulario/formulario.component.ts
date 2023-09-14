fe041577b2f4c1fa08f613183625339c5933b84d
Sebastian Orjuela <sebasorjuela@gmail.com>
2023-09-14 16:08:50 -0500

    Update with the forms complete

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
@@ -11,18 +11,46 @@ export class FormularioComponent implements OnInit {
 
   constructor(private fb: FormBuilder) {
     this.formulario = this.fb.group({
-      nombre: ['', Validators.required],
-      cedula: ['', [Validators.required, Validators.pattern(/^[0-9]{9}$/)]],
-      fechaNacimiento: ['', Validators.required]
+      Placa: ['', [Validators.required], [this.validarPlacaFormatoAsync.bind(this)]],
+      Correo: ['', [Validators.required, Validators.email]],
+      nombre: ['', [Validators.required]],
+      apellido: ['', [Validators.required]],
+      telefono: ['', Validators.pattern('[0-9]{10}')],
     });
   }
 
   ngOnInit(): void {
+
   }
 
   onSubmit() {
     if (this.formulario.valid) {
       console.log(this.formulario.value);
+    } else {
+      this.markFormGroupTouched(this.formulario);
     }
+  }
+  validarPlacaFormatoAsync(control: AbstractControl): Promise<any> | null {
+    return new Promise((resolve) => {
+      const placa = control.value;
+      const placaRegex = /^[A-Z]{3}\d{3}$/; // Ejemplo de formato AAA111
+
+      if (!placaRegex.test(placa)) {
+        resolve({'placaInvalida': true});
+      } else {
+        resolve(null);
+      }
+    });
+  }
+
+
+  markFormGroupTouched(formGroup: FormGroup) {
+    Object.values(formGroup.controls).forEach(control => {
+      control.markAsTouched();
+
+      if (control instanceof FormGroup) {
+        this.markFormGroupTouched(control);
+      }
+    });
   }
 }
