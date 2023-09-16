f1f05baa736daded42ccb9688455d7bf7a7193a0
Sebastian Orjuela <sebasorjuela@gmail.com>
2023-09-16 15:47:12 -0500

    fix some bugs in the plate request

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,25 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
 @Component({
-  selector: 'app-formulario',
-  templateUrl: './formulario.component.html',
-  styleUrls: ['./formulario.component.css']
+  selector: "app-formulario",
+  templateUrl: "./formulario.component.html",
+  styleUrls: ["./formulario.component.css"],
 })
 export class FormularioComponent implements OnInit {
   form: FormGroup;
 
   constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({
-    });
+    this.form = this.fb.group({});
   }
 
   ngOnInit(): void {
-      this.form = this.fb.group({
-        name: ['', Validators.required],
-        last_name: ['', Validators.required],
-        placa: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(6)], this.validarPlacaFormatoAsync],
-        email: ['', [Validators.required, Validators.email]],
-        phone: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
+    this.form = this.fb.group({
+      name: ["", Validators.required],
+      last_name: ["", Validators.required],
+      placa: [
+        "",
+        [Validators.required, Validators.minLength(6), Validators.maxLength(6)],
+        this.validarPlacaFormatoAsync,
+      ],
+      email: ["", [Validators.required, Validators.email]],
+      phone: [
+        "",
+        [
+          Validators.required,
+          Validators.minLength(10),
+          Validators.maxLength(10),
+        ],
+      ],
     });
   }
   onSubmit() {
@@ -34,17 +49,16 @@ export class FormularioComponent implements OnInit {
       const placa = control.value;
       const placaRegex = /^[A-Z]{3}\d{3}$/;
 
-      if (!placaRegex.test(placa)) {
-        resolve({'placaInvalida': true});
+      if (!placaRegex.test(placa.toUpperCase())) {
+        resolve({ placaInvalida: true });
       } else {
         resolve(null);
       }
     });
   }
 
-
   markFormGroupTouched(formGroup: FormGroup) {
-    Object.values(formGroup.controls).forEach(control => {
+    Object.values(formGroup.controls).forEach((control) => {
       control.markAsTouched();
 
       if (control instanceof FormGroup) {
